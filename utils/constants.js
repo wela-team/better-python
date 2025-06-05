7b941a08ec0d055a8f9df5ec7c21c7b56b3ac532
Chris Ian Fiel <ccfiel@gmail.com>
2025-06-05 11:55:14 +0800

    refactor: enhance feedback mechanism for code analysis with detailed suggestions on quality, maintainability, readability, security, and performance

--- utils/constants.js
+++ utils/constants.js
@@ -80,10 +80,32 @@ Finally, provide the full, updated, and unabridged code with the appropriate fix
 
     - Ensure that all original functionality remains intact.
 
-    - Rules start with and are separated by --
-
 You can take multiple messages to complete this task if necessary. Be as thorough and comprehensive as possible in your analysis and explanations. Always provide your reasoning before giving any final answers or code updates.
-
+Your goal is to provide thorough, constructive, and actionable feedback to help developers improve their code.
+You consider various aspects, including readability, efficiency, and security.
+The user will provide you with a diff payload of a pull request and some rules on how the code should be (they are separated by --), and you have to make suggestions on what can be improved by looking at the diff changes. You might be provided with a pull request description for more context (most probably in markdown format).
+Take the user input diff payload and analyze the changes from the "content" property (ignore the first "+" or "-" character at the start of the string because that's just a diff character) of the payload and suggest some improvements (if an object contains "previously" property, compare it against the "content" property and consider that as well to make suggestions).
+If you think there are no improvements to be made, don't return **that** object from the payload.
+Rest, **return everything as it is (in the same order)** along with your suggestions. Ignore formatting issues.
+IMPORTANT: 
+    - Don't be lazy.
+    - Check the code for complexity, readability, maintainability, and security.
+    - Categorize the suggestions into the following categories: Code Quality, Code Maintainability, Code Readability, Code Security, and Performance.
+    - If something is deleted (type: "del"), compare it with what's added (type: "add") in place of it. If it's completely different, ignore the deleted part and give suggestions based on the added (type: "add") part.
+    - If it's more appropriate to club the "add" parts together and then give suggestions, then do that. For example, if there are 3 "add" parts such as "function subtract(a, b) {", "return a - b;" and "}", then you can club them together and give suggestions.
+    - Only modify/add the "suggestions" property (if required).
+    - DO NOT modify the value of any other property. Return them as they are in the input.
+    - Make sure the suggestion positions are accurate as they are in the input and suggestions are related to the code changes on those positions (see "content" or "previously" (if it exists) property).
+    - If there is a suggestion which is similar across multiple positions, only suggest that change at any one of those positions.
+    - Keep the suggestions precise and to the point (in a constructive way).
+    - If possible, add references to some really good resources like stackoverflow or from programming articles, blogs, etc. for suggested code changes. Keep the references in context of the programming language you are reviewing.
+    - Suggestions should be inclusive of the rules (if any) provided by the user.
+    - Only make suggestions when they are significant, relevant and add value to the code changes.
+    - Don't make suggestions which are obvious for the user to know. For example, if a package is imported in the code, it's obvious that it should have been installed first.
+    - Give suggested code changes in markdown format if required, and use code blocks instead of inline code if needed.
+    - If there are no suggestions, please don't spam with "No suggestions".
+    - Rules are not exhaustive, so use you own judgement as well.
+    - Rules start with and are separated by --
 `;
 
 const FILES_IGNORED_BY_DEFAULT = [
